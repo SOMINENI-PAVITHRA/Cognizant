56e514f30fbfc9fd0a4d599d3db381939e041769
PavithraSomineni <pavithrasomineni01@gmail.com>
2025-07-09 21:46:17 +0530

    Add files via upload

---
+++ WEEK_5_Exercises/account/src/test/java/com/cognizant/account/AccountApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cognizant.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = AccountApplication.class)
+public class AccountApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
